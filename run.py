ea62ef5eaee74cbb711909efdb45ea9cdb5ba005
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-02-21 02:10:28 +0300

    server up

---
+++ run.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+from blueprints.tauri_releases import tauri_releases_bp
+# Import the blueprint from routes/tauri_releases_bp.py
+
+app = Flask(__name__)
+CORS(app)
+try:
+    app.register_blueprint(tauri_releases_bp)
+except Exception as e:
+    print(e)
+if __name__ == '__main__':
+    app.run()
